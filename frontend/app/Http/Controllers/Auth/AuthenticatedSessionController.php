cdbb4ddd87367e35d851bce171dadbeb4e4f7ce2
cybertcode <cybertcode@gmail.com>
2023-04-24 18:30:24 -0500

    ✨ Iniciar sesión desde el cliente

--- frontend/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ frontend/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -3,11 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Auth\LoginRequest;
-use App\Providers\RouteServiceProvider;
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
 use Illuminate\View\View;
 
 class AuthenticatedSessionController extends Controller
@@ -23,13 +23,26 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): RedirectResponse
+    // public function store(LoginRequest $request): RedirectResponse //Anterior
+    public function store(Request $request)
     {
-        $request->authenticate();
-
-        $request->session()->regenerate();
-
-        return redirect()->intended(RouteServiceProvider::HOME);
+        //Anterior
+        // $request->authenticate();
+        // $request->session()->regenerate();
+        // return redirect()->intended(RouteServiceProvider::HOME);
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+        $response = Http::withHeaders([
+            'Accept' => 'application/json',
+        ])->post('http://127.0.0.1:8000/v1/login', [
+            'email' => $request->email,
+            'password' => $request->password]);
+        // return $response->json();
+        $response = $response->json();
+        $user = User::updateOrcreate(['email' => $request->email], $response['data']);
+        return $user;
     }
 
     /**
--- routes/api-v1.php
+++ routes/api-v1.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\LoginController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\PostController;
 use App\Http\Controllers\Api\RegisterController;
