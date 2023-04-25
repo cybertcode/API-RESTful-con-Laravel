96d01033de49d54e9a5276f527eafbac7aef9d9a
cybertcode <cybertcode@gmail.com>
2023-04-25 10:34:47 -0500

    ✨ Registrar usuario desde el cliente II

--- frontend/app/Http/Controllers/Auth/RegisteredUserController.php
+++ frontend/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -9,8 +9,8 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Validation\Rules\Password;
 use Illuminate\View\View;
 
 class RegisteredUserController extends Controller
@@ -32,14 +32,33 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
+            'password' => ['required', 'confirmed', Password::defaults()],
         ]);
-
+        $response = Http::acceptJson()->post('http://127.0.0.1:8000/v1/register', $request->all());
+        if ($response->status() == 422) {
+            return back()->withErrors($response->json()['errors']);
+        }
+        $service = $response->json();
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => Hash::make($request->password),
+            // 'password' => Hash::make($request->password),
+        ]);
+        // Para el token
+        $response = Http::acceptJson()->post('http://127.0.0.1:8000/oauth/token', [
+            'grant_type' => 'password',
+            'client_id' => '99029efc-1374-4e55-9070-535d099594c7',
+            'client_secret' => 'j7HhxX6ZJtCcRDTkd3s2oNC2xWFslyVebPZPLTCh',
+            'username' => $request->email,
+            'password' => $request->password,
+        ]);
+        $access_token = $response->json();
+        $user->accessToken()->create([
+            'service_id' => $service['data']['id'],
+            'access_token' => $access_token['access_token'],
+            'refresh_token' => $access_token['refresh_token'],
+            'expires_at' => now()->addSecond($access_token['expires_in']),
         ]);
 
         event(new Registered($user));
